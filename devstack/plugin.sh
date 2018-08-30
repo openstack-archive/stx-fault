1ecff391836c3046f1c2aa762f482a4a88373a34
Dean Troyer <dtroyer@gmail.com>
2018-08-30 14:03:59 -0500

    Add a basic devstack job
    Begin working on a base devstack job for testing stx projects.
    We have to pull a lot of stable/pike bits yet let STX services
    run from master.
    
    Change-Id: I6eee854888acb27b54c10c25d47e97d98a7f999c
    Signed-off-by: Dean Troyer <dtroyer@gmail.com>

--- devstack/plugin.sh
+++ devstack/plugin.sh
@@ -1,34 +1,31 @@
 #!/bin/bash
 
 # devstack/plugin.sh
-# Triggers stx_fault specific functions to install and configure Fault Management
+# Triggers stx-fault specific functions to install and configure Fault Management
 
-# Dependencies:
-#
-# - ``functions`` file
-# - ``DATA_DIR`` must be defined
-
-# ``stack.sh`` calls the entry points in this order:
-#
 echo_summary "fault devstack plugin.sh called: $1/$2"
-source $DEST/stx-fault/devstack/lib/stx-fault
 
 # check for service enabled
-if [[ ,${ENABLED_SERVICES} =~ ,"fm-" ]]; then
+if is_service_enabled stx-fault; then
+    if [[ "$1" == "source" ]]; then
+        # Initial source of lib script
+        source $(dirname "$0")/lib/stx-fault
+    fi
+
     if [[ "$1" == "stack" && "$2" == "install" ]]; then
         # Perform installation of source
-        echo_summary "Installing fault service"
+        echo_summary "Instal stx-fault"
         install_fault
 
     elif [[ "$1" == "stack" && "$2" == "post-config" ]]; then
         # Configure after the other layer 1 and 2 services have been configured
-        echo_summary "Configuring fault"
+        echo_summary "Configure stx-fault"
         configure_fault
         create_fault_user_group
         create_fault_accounts
     elif [[ "$1" == "stack" && "$2" == "extra" ]]; then
         # Initialize and start the service
-        echo_summary "Initializing and start fault"
+        echo_summary "Initialize and start stx-fault"
         init_fault
         start_fault
     elif [[ "$1" == "stack" && "$2" == "test-config" ]]; then
@@ -38,13 +35,11 @@ if [[ ,${ENABLED_SERVICES} =~ ,"fm-" ]]; then
 
     if [[ "$1" == "unstack" ]]; then
         # Shut down services
-        echo_summary "Stop fault service"
+        echo_summary "Stop stx-fault services"
         stop_fault
-        :
     fi
 
     if [[ "$1" == "clean" ]]; then
         cleanup_fault
-        :
     fi
 fi
