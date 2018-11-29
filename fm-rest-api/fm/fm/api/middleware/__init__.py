d6e96be431bde99362820f8c0b0eeaff07245807
Shuicheng Lin <shuicheng.lin@intel.com>
2018-11-29 23:14:51 +0800

    Enable fm-rest-api to return json format error message
    Closes-Bug: 1789979
    
    fm-rest-api returns json format message for the correct api
    access. For the invalid api access, the message is with html
    format, need add wrap_app to convert it to json format.
    Fix:
    The implementation code is copied upstream Ironic API, and
    minor modifcation for fm and tox pep8.
    Also remove "x" attribute for the py file.
    Test:
    Pass build and multi node deploy test. Confirmed the return
    message is json format.
    
    Change-Id: I36fa89b82377d52008a467316c42c06caa65cd90
    Signed-off-by: Shuicheng Lin <shuicheng.lin@intel.com>

--- fm-rest-api/fm/fm/api/middleware/__init__.py
+++ fm-rest-api/fm/fm/api/middleware/__init__.py
@@ -3,3 +3,18 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+from fm.api.middleware import auth_token
+from fm.api.middleware import parsable_error
+
+
+ParsableErrorMiddleware = parsable_error.ParsableErrorMiddleware
+AuthTokenMiddleware = auth_token.AuthTokenMiddleware
+
+__all__ = (ParsableErrorMiddleware,
+           AuthTokenMiddleware)
