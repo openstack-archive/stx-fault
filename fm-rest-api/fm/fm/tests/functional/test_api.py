aec83ac3dede39183d352c2a88511ddffd2ecd7f
Dean Troyer <dtroyer@gmail.com>
2019-03-29 12:44:39 -0500

    Add gabbi tests to fm-rest-api functional tests
    This adds a basic REST API test framework built around gabbi[0]
    that acts as a starting point for REST API tests and validation
    that the fm-rest-api service is running and responsive.
    
    It adds the basic setup requirements for running tox tests
    in fm-rest-api/fm to the 'functional' tox environment.
    
    OS_TEST_URL points to the faultmanagement endpoint, until we
    learn how to get it from the service catalog.
    
    [0] https://github.com/cdent/gabbi
    
    Change-Id: I7f73848fbee4899e70a66e68541712f405f12a44
    Signed-off-by: Dean Troyer <dtroyer@gmail.com>

---
+++ fm-rest-api/fm/fm/tests/functional/test_api.py
@@ -0,0 +1,44 @@
+# Copyright 2019 Intel Corporation
+#
+# SPDX-License-Identifier: Apache-2.0
+
+"""REST API test loader"""
+
+import os
+
+from gabbi import driver
+import openstack
+
+# Location of the YAML files that define gabbi tests
+TESTS_DIR = 'gabbits'
+
+# The environment variable with the bare test endpoint
+# TODO(dtroyer): Teach this how to get the endpoint from the service catalog
+TEST_URL = "OS_TEST_URL"
+
+
+def _get_endpoint():
+    """Get the service URL from the service catalog"""
+    conn = openstack.connect()
+    ep = conn.config.get_session_endpoint('faultmanagement')
+    if (ep.endswith("/v1")):
+        ep = ep.replace("/v1", "")
+    return ep
+
+
+def load_tests(loader, tests, pattern):
+    """Provide a TestSuite to the discovery process."""
+    test_dir = os.path.join(os.path.dirname(__file__), TESTS_DIR)
+    test_url = os.environ.get(TEST_URL, None)
+    if test_url is None:
+        test_url = _get_endpoint()
+
+    # Pass "require_ssl=True" as an argument to force all tests
+    # to use SSL in requests.
+    return driver.build_tests(
+        test_dir,
+        loader,
+        url=test_url,
+        # intercept=wsgiapp.app,
+        # fixture_module=fixtures,
+    )
